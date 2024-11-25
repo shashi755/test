d4d87643d85d169e9c1858a6656868c60f0a4945
shashi755 <shashivardhanreddythatiparthi@gmail.com>
2024-11-26 01:27:57 +0530

    added 3 files

---
+++ Main.java
@@ -0,0 +1,6 @@
+// Save this file as HelloWorld.java
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); // Print "Hello, World!" to the console
+    }
+}
---
+++ demo.py
@@ -0,0 +1,2 @@
+# Save this file as hello.py
+print("Hello, World!")  # Print "Hello, World!" to the console
